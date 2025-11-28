2b732d0c3de4730daae18ee47ef742782dcd105f
FranciscoMorales1 <146593362+FranciscoMorales1@users.noreply.github.com>
2025-11-28 11:22:24 -0500

    Delete bio directory

--- bio/BIOErrorListener.py
+++
@@ -1,8 +0,0 @@
-from antlr4.error.ErrorListener import ErrorListener
-
-class BIOErrorListener(ErrorListener):
-
-    def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
-        print(f"[Error de sintaxis] Línea {line}, columna {column}: {msg}")
-        # Evita que ANTLR siga parseando basura
-        raise Exception("ErrorSintaxis")
